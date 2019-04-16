d2857d60a95392ce9070a116aad98edc87cd43d5
Felipe Ryan <fryan@fryan-ltm3.internal.salesforce.com>
2019-04-16 15:59:07 +0200

    needs to be decorated

--- force-app/main/default/lwc/conversationLWC/conversationLWC.js
+++ force-app/main/default/lwc/conversationLWC/conversationLWC.js
@@ -1,5 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ConversationLWC extends LightningElement {
-    conversation_cat;
+    @api conversation_cat;
 }
